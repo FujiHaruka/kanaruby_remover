d44cc4abb0d0aebbd4aa334f043742a1924f0147
FujiHaruka <fujiharuka@realglobe.jp>
2017-11-19 16:37:24 +0900

    Add logger

--- bin/kanaruby-remover.py
+++ bin/kanaruby-remover.py
@@ -3,6 +3,8 @@ import sys
 import os
 import argparse
 import cv2
+import logging
+
 
 package_path = os.path.dirname(os.path.abspath(__file__)) + "/../kanaruby_remover"
 sys.path.append(package_path)
@@ -13,10 +15,14 @@ parser = argparse.ArgumentParser(description='Remove kana rubies')
 
 parser.add_argument('src', type=str, help='Source image path')
 parser.add_argument('dest', type=str, help="Destination image path")
+parser.add_argument('--log', type=str, help="Log level", default="INFO")
 
 args = parser.parse_args()
 src = args.src
 dest = args.dest
+loglevel = args.log
+
+logging.basicConfig(level=getattr(logging, loglevel))
 
 # Main
 image = cv2.imread(src)
--- kanaruby_remover/remover.py
+++ kanaruby_remover/remover.py
@@ -2,11 +2,14 @@ import math
 import cv2
 import numpy as np
 from histgram import Histgram
+from logging import getLogger
+logger = getLogger(__name__)
 
 def cvt_gray(image):
     return cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
 def straighten(image):
+    logger.debug('straighten...')
     gray = cvt_gray(image)
     # Gaussian , threshold, contours
     sigma_x = 65
@@ -26,12 +29,15 @@ def straighten(image):
         angle += 90
     while angle >= 45:
         angle -= 90
+    logger.debug('Largest contour angle: {}'.format(angle))
+
     rows, cols, _ = image.shape
     rotationMat = cv2.getRotationMatrix2D((cols/2, rows/2), angle, 1)
     res_image = cv2.warpAffine(image, rotationMat, (cols, rows), borderMode=cv2.BORDER_CONSTANT, borderValue=(255,255,255))
     return res_image
 
 def shadow_y(image):
+    logger.debug('shadow_y...')
     # gaussian_y
     sigma_x = 1
     sigma_y = 127
@@ -44,6 +50,7 @@ def shadow_y(image):
     return thres
 
 def detect_rubies(shadow_image):
+    logger.debug('detect_rubies...')
     # 連結成分を取得する
     nlabels, labelimg, contours, CoGs = cv2.connectedComponentsWithStats(shadow_image)
 
@@ -52,11 +59,13 @@ def detect_rubies(shadow_image):
     max_width = get_width(max(contours, key=get_width))
     widths = [get_width(c) for c in contours if get_width(c) != max_width]
     w_hist = Histgram(widths)
+    logger.debug('histgram {}'.format(w_hist))
 
     # ヒストグラムの中点以下をルビと考える
     # 小さすぎるものはノイズ
     half_size = math.floor(w_hist.size / 2)
     ruby_contours = [c for c in contours if w_hist.get_index(get_width(c)) < half_size and w_hist.get_index(get_width(c)) > 0]
+    logger.debug('Detect {} rubies contours'.format(len(ruby_contours)))
     return ruby_contours
 
 def remove_rubies_by_contours(image, ruby_contours, color=(255,255,255)):
@@ -66,8 +75,8 @@ def remove_rubies_by_contours(image, ruby_contours, color=(255,255,255)):
         cv2.rectangle(image, (x,0), (x+w,image_h), color, -1)
 
 def remove_rubies(image):
-    straigt_image = straighten(image)
-    shadow_image = shadow_y(straigt_image)
+    straight_image = straighten(image)
+    shadow_image = shadow_y(straight_image)
     rubies = detect_rubies(shadow_image)
-    remove_rubies_by_contours(straigt_image, rubies)
-    return straigt_image
+    remove_rubies_by_contours(straight_image, rubies)
+    return straight_image
